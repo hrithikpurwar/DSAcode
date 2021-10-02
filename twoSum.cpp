ae868ccbc3b793cb4d9d7bf43ac0b5a427f862be
Raftaar-singh <91781805+Raftaar-singh@users.noreply.github.com>
2021-10-02 15:36:29 +0530

    Update twoSum.cpp

--- twoSum.cpp
+++ twoSum.cpp
@@ -13,6 +13,7 @@ int main(){
     cout<<"enter the target number:";
     cin>> targ;
     for(int i=0; i<n; i++){
+        // this is important change
         for(int j=i; j<n; j++){
             int sum = arr[i]+arr[j];
             if(sum==targ){
@@ -22,21 +23,3 @@ int main(){
     }
     return 0;
 }
-
-//TODO: submited this one only
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> ans;
-//         for(int i=0;i<nums.size();i++){
-//             for(int j=i+1;j<nums.size();j++){
-//                 if(nums[i]+nums[j]==target){
-//                     ans.push_back(i);
-//                     ans.push_back(j);
-//                     break;
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
