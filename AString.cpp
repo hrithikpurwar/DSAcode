7411a35fa522d9b28fabc9ff9f3dc01cee8e98e4
Satyajeet Ramnit <75561539+satyajeetramnit@users.noreply.github.com>
2021-10-02 17:31:11 +0530

    Update AString.cpp

--- AString.cpp
+++ AString.cpp
@@ -6,15 +6,6 @@
 using namespace std;
 
 int main(){
-
-    // DIFFRENT WAY TO TAKE INPUT IN STRING
-    // string str1(5, 'n');
-    // cout << str1 << endl;
-
-    // string str2 = "AnkitRaj"; <!-- you can see we are not using space between Ankit and Raj -->
-    // cout << str2;
-
-    //this is to take input of a whole sentence
     string str3;
     getline(cin, str3);
     cout << str3 << endl;
