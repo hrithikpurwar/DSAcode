44d00ddcec167bc9a76a1b4769266491e5eb531f
SatyajeetRamnit27 <91786741+SatyajeetRamnit27@users.noreply.github.com>
2021-10-02 18:02:05 +0530

    Update compare.cpp

--- compare.cpp
+++ compare.cpp
@@ -11,8 +11,9 @@ int main(){
     if(!s1.compare(s2)) //or you can write if(s1.compare(s2) == 0)
         cout<< "STRINGS ARE EQUAL" <<endl;
 
-    // cout << s2.compare(s1) << endl;
+    // cout << s2.compare(s1) << endl;//agar chaiye to ye bhi kar sakte hai
     //agar 1 aata hai too s2 bara hai basically postive aata hai too bara hai aur 0(zero) aata hai 
+    //sahi baat hai
     //too equal hai
 
     return 0;
