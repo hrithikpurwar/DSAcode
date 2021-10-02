5954499447fa35357a6636161e94dd1df3dc85b5
Raftaar-singh <91781805+Raftaar-singh@users.noreply.github.com>
2021-10-02 15:37:23 +0530

    Update find.cpp

--- find.cpp
+++ find.cpp
@@ -7,6 +7,7 @@ int main(){
     string s1 = "nincompoop";
 
      //find() --> it direct return the index of element we are finding 
+    //this is important change
 
     cout<< s1.find("poo") <<endl;
     return 0;
